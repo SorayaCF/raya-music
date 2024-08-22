054054eca97371372ab3e87cbbdeb7c8211e1e5d
SorayaCF <soraya@MacBook-Pro-de-Soraya.local>
2024-08-22 16:40:42 +0200

    dark mode try

--- app/_components/intro.jsx
+++ app/_components/intro.jsx
@@ -11,7 +11,7 @@ export default function Intro() {
                         Descubre lo último de tu grupo favorito.
                     </p>
                     <a href='https://soundcloud.com/laura-raya' target='_blank'>
-                        <button className="bg-gradient-to-r hover:from-teal-500 hover:to-blue-500 from-pink-500 to-orange-500 text-white px-6 py-3 rounded-full text-lg flex gap-2 items-center">
+                        <button className="bg-gradient-to-r hover:from-teal-500 hover:to-blue-500 from-pink-500 to-orange-500 text-white dark:text-red-500 px-6 py-3 rounded-full text-lg flex gap-2 items-center">
                             <div>Escuchar en SoundCloud</div>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
